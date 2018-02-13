e462fb058b06ff74a4db60e6c6d3722e61217dcc
Nicki Lang <pandaunderwear@gmail.com>
2018-02-13 11:24:41 -0800

    Made a few changes, still needs work.

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ var $searchBtn = document.querySelector("#search");
 
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
 var filteredDate = datetimeData;
 
-// renderTable renders the filteredAddresses to the tbody
 function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredDate.length; i++) {
-    // Get get the current address object and its fields
     var date = filteredDate[i];
     var fields = Object.keys(date);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
       $cell.innerText = date[field];
@@ -25,6 +21,14 @@ function renderTable() {
   }
 }
 
+function handleSearchButtonClick() {
+    var filterDate = $dateInput.value.toString();
+    filterDate = datetimeData.filter(function(date) {
+        var dataDate = date.datetime.toString();
+        return dataDate === filterDate;
+    });
+    renderTable();
+}
   
   // Render the table for the first time on page load
   renderTable();
